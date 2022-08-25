3b811d99efcdc59a30a37641c8bbd443edefffc8
Raimar Tontsch <raimar.tontsch@gmail.com>
2022-08-25 10:38:00 +0200

    Add styled components plugin

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import Nav from './Components/Nav'
-import GlobalStyle from './Components/UI'
 
 function App() {
 
--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,10 +1,26 @@
+
 export default function Nav() {
+
+
 	return (
-		<div>
-			<p>About me</p>
-			<p>CV</p>
-			<p>Projekte</p>
-			<p>Social Media </p>
+		<div className="nav-box">
+
+			<h2>About me</h2>
+			<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Excepturi sapiente repudiandae tempore ex fugit placeat optio? Nobis enim, deleniti fuga nisi tenetur dolor aut. Quasi ab unde esse eius aut!</p>
+			
+			<h2>CV</h2>
+			<ul>
+			<li>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Excepturi sapiente repudiandae tempore ex fugit placeat optio? Nobis enim, deleniti fuga nisi tenetur dolor aut. Quasi ab unde esse eius aut!</li>
+			</ul>
+
+			<h2>Projekte</h2>
+			<ul>
+			<li>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Excepturi sapiente repudiandae tempore ex fugit placeat optio? Nobis enim, deleniti fuga nisi tenetur dolor aut. Quasi ab unde esse eius aut!</li>
+			</ul>
+
+			<h2>Social Media </h2>
+			<ul><li>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Excepturi sapiente repudiandae tempore ex fugit placeat optio? Nobis enim, deleniti fuga nisi tenetur dolor aut. Quasi ab unde esse eius aut!</li>
+			</ul>
 		</div>
 	);
 }
