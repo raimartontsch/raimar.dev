ab44e5b183bc4816d5445301db9028ec323de292
Raimar Tontsch <raimar.tontsch@gmail.com>
2022-09-23 14:07:07 +0200

    Add animation

--- src/Components/UI/GlassContainer.styled.js
+++ src/Components/UI/GlassContainer.styled.js
@@ -5,7 +5,6 @@ export const GlassContainer = styled.div`
 	align-items: center;
 	align-self: center;
 	justify-content: center;
-	//border: 1px solid rgba(255, 255, 255, 0.5);
 	border-radius: 16px;
 	overflow: hidden;
 
@@ -14,6 +13,4 @@ export const GlassContainer = styled.div`
 	left: 0;
 	right: 0;
 	bottom: 0;
-	//background: inset 0 0 200px rgba(255, 255, 255, 0.11);
-	//backdrop-filter: blur(5px);
 `;
